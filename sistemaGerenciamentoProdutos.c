b6712ddb7028ff338b63e0728c45e8e66beff44c
Kleber Petry <kleberpetry@gmail.com>
2022-10-13 22:25:41 -0300

    1

--- sistemaGerenciamentoProdutos.c
+++ sistemaGerenciamentoProdutos.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+struct produto{
+	int codigo;
+	char nome[30];
+	double preco;
+	int estoque;
+};
+struct produto produtos[200];
+int sequencia=0;
+
+void cadastro(){
+	system("cls");
+	produtos[sequencia].codigo=sequencia+1;
+	printf("\nDigitie o nome do produto: ");
+	fflush(stdin);
+	gets(produtos[sequencia].nome);
+	printf("\nDigite o preço: ");
+	scanf("%lf",&produtos[sequencia].preco);
+	printf("\nDigite o estoque inicial: ");
+	scanf("%d",&produtos[sequencia].estoque);
+	sequencia++;
+}
+void listar(){
+	
+}
+void venda(){
+	
+}
 
 void menu(){
 	printf("\nSistema de gerenciamento");
