b0ed583418f5482e5ac46b9050785f48e74ad589
Kleber Petry <kleberpetry@gmail.com>
2022-10-20 22:22:51 -0300

    aula 20-10-22

--- sistemaGerenciamentoProdutos.c
+++ sistemaGerenciamentoProdutos.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+#include<stdbool.h>
 struct produto{
 	int codigo;
 	char nome[30];
@@ -23,8 +24,40 @@ void cadastro(){
 	sequencia++;
 }
 void listar(){
-	
+	system("cls");
+	int x;
+	for(x=0;x<sequencia;x++){
+		printf("\nCodigo: %d",produtos[x].codigo);
+		printf("\nNome: %s",produtos[x].nome);
+		printf("\nPreço: %.2lf",produtos[x].preco);
+		printf("\nEstoque: %d",produtos[x].estoque);
+		printf("\n____________________________\n");
+	}
+	system("pause");
 }
+bool buscar(int codigo){
+	bool tem;
+	if(produtos[codigo-1].codigo==codigo){
+		tem=true;
+	}else{
+		tem=false;
+	}
+	return tem;
+}
+bool verificaEstoque(int codigo,int quantidade){
+	bool tem=false;
+	if(buscar(codigo)==true){
+		if(produtos[codigo-1].estoque>=quantidade){
+			tem=true;
+		}else{
+			tem=false;
+		}
+	}else{
+		
+	}
+	return tem;
+}
+
 void venda(){
 	
 }
@@ -34,17 +67,45 @@ void menu(){
 	printf("\n1-Cadastro de produtos");
 	printf("\n2-Listar produtos");
 	printf("\n3-Realizar venda");
+	printf("\n4-busca produto");
 	printf("\n0-Sair\n");
 }
 
 main(){
 	setlocale(LC_ALL, "Portuguese");
-	int opcao;
+	int opcao,codigo;
 	do{
 		system("cls");
 		menu();
 		scanf("%d",&opcao);
-		
+		switch(opcao){
+			case 1:
+				cadastro();
+			break;
+			case 2:
+				listar();
+			break;
+			case 3:
+				printf("ok3");
+				system("pause");
+			break;
+			case 4:
+				printf("\nDigite o código do produto: ");
+				scanf("%d",&codigo);
+				if(buscar(codigo)==true){
+					printf("\nproduto existe");
+				}else{
+					printf("\nproduto não existe");
+				}
+				system("pause");
+			break;
+			case 0:
+				printf("ok0");
+				system("pause");
+			break;
+			default:
+				printf("\nOpção inválida!\n");
+		}
 	}while(opcao!=0);
 	printf("\nAplicação encerrada\n");
 	
